38c2120d56187089ae699931ef28ea9630927ed6
vanderleipinto <vandecopinto@gmail.com>
2023-05-15 14:45:38 -0300

    Projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { GridItem } from "./components/GridItem";
 
 import { GridItemType } from "./types/GridItemType";
 import { items } from "./data/items";
+import { formatTimeElapsed } from "./helpers/formatTimeelapsed";
 
 const App = () => {
   const [playing, setPlaying] = useState<boolean>(false);
@@ -18,6 +19,57 @@ const App = () => {
   const [gridItems, setGridItems] = useState<GridItemType[]>([]);
 
   useEffect(() => resetAndCreateGrid(), []); //Roda a função ao carregar a página.
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (playing) setTimeElapsed(timeElapsed + 1);
+    }, 1000);
+    return () => clearInterval(timer); // Return dentro de useEffect é executado quando o programa é finalizado ou atualizado.
+  }, [playing, timeElapsed]); // Aqui no array final, temos o play e o timeElapsed que são as variáveis sendo observadas pelo useEffect
+
+  // verifica se os abertos são iguais
+  useEffect(() => {
+    if (shownCount === 2) {
+      let opened = gridItems.filter((item) => item.shown === true); // cria um array somente com os items que estão abertos.
+      if (opened.length === 2) {
+        // verifica se  eles são iguais, torná-los permanentes
+        if (opened[0].item === opened[1].item) {
+          let tmpGrid = [...gridItems];
+          for (let i in tmpGrid) {
+            if (tmpGrid[i].shown) {
+              tmpGrid[i].permanentShown = true;
+              tmpGrid[i].shown = false;
+            }
+            setGridItems(tmpGrid);
+            setShownCount(0);
+          }
+        } else {
+          // não são iguais, shown é false
+          setTimeout(() => {
+            let tmpGrid = [...gridItems];
+            for (let i in tmpGrid) {
+              tmpGrid[i].shown = false;
+            }
+            setGridItems(tmpGrid);
+            setShownCount(0);
+          }, 1000); // aguarda um segundo mostrando eles errados depois faz a correção.
+        }
+
+        setMoveCount((moveCount) => moveCount + 1);
+      }
+    }
+  }, [shownCount, gridItems]);
+
+  // verifica se deu game over
+  useEffect(() => {
+    if (
+      moveCount > 0 &&
+      gridItems.every((item) => item.permanentShown === true)
+    ) {
+      // every verifica se todos os items são true.
+      setPlaying(false);
+    }
+  }, [moveCount, gridItems]); // se um desses items modificar ele roda a funçao do useEffect
 
   const resetAndCreateGrid = () => {
     //Passo 1 - Resetar o jogo
@@ -50,7 +102,23 @@ const App = () => {
     setPlaying(true);
   };
 
-  const handleItemClick = (index: number) => {};
+  const handleItemClick = (index: number) => {
+    if (playing && index !== null && shownCount < 2) {
+      // jogo tá rolando? a partir de showcount 2 não pode exibir o terceiro item
+      let tmpGrid = [...gridItems]; // cria o clone do array de objeto depois muda lá no setgriditems
+
+      if (
+        tmpGrid[index].permanentShown === false &&
+        tmpGrid[index].shown === false
+      ) {
+        // o item está de costas
+        tmpGrid[index].shown = true;
+        setShownCount(shownCount + 1);
+      }
+
+      setGridItems(tmpGrid);
+    }
+  };
 
   return (
     <C.Container>
@@ -58,9 +126,10 @@ const App = () => {
         <C.LogoLink href="">
           <img src={logoImage} width={200} alt="" />
         </C.LogoLink>
-        <C.InfoArea>InfoArea</C.InfoArea>
-        <InfoItem label="Tempo" value="00:00" />
-        <InfoItem label="Movimentos" value="0" />
+        <C.InfoArea>
+          <InfoItem label="Tempo" value={formatTimeElapsed(timeElapsed)} />
+          <InfoItem label="Movimentos" value={moveCount.toString()} />
+        </C.InfoArea>
         <Button
           label="Reiniciar"
           icon={restartIcon}
---
+++ src/helpers/formatTimeelapsed.ts
@@ -0,0 +1,11 @@
+/* Essa função formata os valores de gerados no setinterval (segundos) para minutos e segundos */
+
+export const formatTimeElapsed = (seconds: number) => {
+  let minutes = Math.floor(seconds / 60);
+  seconds = seconds - minutes * 60;
+
+  let secString = `${seconds < 10 ? "0" + seconds : seconds}`;
+  let minString = `${minutes < 10 ? "0" + minutes : minutes}`;
+
+  return `${minString}:${secString}`;
+};
